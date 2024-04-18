e6ac6fe5c69d4bd0afb92d90af279e9a9e8ba4a9
crin706 <rosh-ka@mail.ru>
2024-04-18 22:57:41 +0300

    Fired IT

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int toadEyesCount = 0;          //Жабьи глаза
-        int ghoulTearsCount = 0;        //Слезы вурдалака
-        int ravenBonesCount = 0;        //Кости ворона
-        int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
+        Scanner scan = new Scanner(System.in);
+//        int toadEyesCount = 0;          //Жабьи глаза
+//        int ghoulTearsCount = 0;        //Слезы вурдалака
+//        int ravenBonesCount = 0;        //Кости ворона
+//        int dumplingsCount = 0;         //Пельмени(а почему, собственно нет? xD )
 
         //todo реализовать ввод пользователем кол-ва ингредиентов.
+        System.out.println("Введите количество жабьих глаз: ");
+        int toadEyesCount = scan.nextInt();
+        System.out.println("Введите количество костей ворона: ");
+        int ghoulTearsCount = scan.nextInt();
+        System.out.println("Введите количество слез вурдалака: ");
+        int ravenBonesCount = scan.nextInt();
+        System.out.println("Введите количество пельмешек: ");
+        int dumplingsCount = scan.nextInt();
+
 
         //Рецепты Эликсиров:
         // 1. 3 Жабьих глаза + 1 кость ворона - Эликсир зоркости
@@ -14,5 +26,12 @@ public class Main {
         // 4. 5 слез вурдалака + 10 пельменей + 4 жабьих глаза + 3 кости ворона - Запретный эликсир
 
         //todo дописать логику программы сюда
+        for (toadEyesCount; toadEyesCount > 0; toadEyesCount=+3)
+
+        if (toadEyesCount / 3 > 0 && ravenBonesCount / 1 > 0) {
+            System.out.println("Вы можете приготовить: " + toadEyesCount / 3 + " шт эликсиров зоркости.");
+        } else {
+            System.out.println("Недостаточно ингредиентов");
+        }
     }
 }
--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+        int itWorker = 0;
+        while (itWorker < 100) {
+            itWorker += 2;
+            System.out.println("Уволен айтишник с id " + itWorker);
+        }
     }
 }
